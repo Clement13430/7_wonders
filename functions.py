23d3a0002c0354d6489d5dd51ae4b73d48d24637
Clement13430 <clement13430@gmail.com>
2025-07-02 10:05:09 +0200

    :tada: Initialize the project.

---
+++ functions.py
@@ -0,0 +1,20 @@
+import copy
+
+def enough_resources(resources_available, resources_needed) -> bool:
+		resource_left_to_check = copy.copy(resources_needed)
+		resources_left_available = copy.copy(resources_available)
+		i = 0
+		if len(resource_left_to_check) == 0:
+			return True
+		while i < len(resource_left_to_check):
+			j = 0
+			while j < len(resources_left_available):
+				if resource_left_to_check[i] == resources_left_available[j]:
+					resources_left_available.pop(j)
+					resource_left_to_check.pop(i)
+					if len(resource_left_to_check) == 0:
+						return True
+					j = 0
+				else:
+					j += 1
+			return False
---
+++ test.py
@@ -0,0 +1,7 @@
+i = 2
+
+def u(integer):
+    if integer<3:
+        raise Exception(f'The lengh of the half ressources is too long: {i}') 
+
+u(i)
